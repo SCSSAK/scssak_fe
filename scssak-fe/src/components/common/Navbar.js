4bd9576b054157db9adc06ebcf935f5273d4d832
정내혁 <math_star@naver.com>
2024-11-13 16:56:24 +0900

    feat: Navbar 이미지 삽입 및 활성화 시 파란색으로 변경 기능 241113 1656

--- scssak-fe/src/components/common/Navbar.js
+++ scssak-fe/src/components/common/Navbar.js
@@ -1,6 +1,14 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import {useNavigate} from 'react-router-dom';
 import '../../styles/components/common/Navbar.css'; // 네비게이션 바 CSS
+import communityIconActive from '../../assets/images/navbar/community_icon_active.png';
+// import communityIconInactive from '../../assets/images/navbar/community_icon_inactive.png';
+import homeIconActive from '../../assets/images/navbar/home_icon_active.png';
+// import homeIconInactive from '../../assets/images/navbar/home_icon_inactive.png';
+import mailboxIconActive from '../../assets/images/navbar/mailbox_icon_active.png';
+// import mailboxIconInactive from '../../assets/images/navbar/mailbox_icon_inactive.png';
+import mypageIconActive from '../../assets/images/navbar/mypage_icon_active.png';
+// import mypageIconInactive from '../../assets/images/navbar/mypage_icon_inactive.png';
 
 const Navbar = () => {
   const navigate = useNavigate();
@@ -8,20 +16,16 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="nav-item" onClick={() => navigate('/home')}>
-        <img src="/icons/home.png" alt="Home" />
-        <span>홈</span>
+        <img src={homeIconActive} alt="Home" className="active" />
       </div>
       <div className="nav-item" onClick={() => navigate('/community')}>
-        <img src="/icons/community.png" alt="Community" />
-        <span>커뮤니티</span>
+        <img src={communityIconActive} alt="Community" />
       </div>
       <div className="nav-item" onClick={() => navigate('/mailbox')}>
-        <img src="/icons/mailbox.png" alt="Mailbox" />
-        <span>우체통</span>
+        <img src={mailboxIconActive} alt="Mailbox" />
       </div>
       <div className="nav-item" onClick={() => navigate('/my')}>
-        <img src="/icons/my.png" alt="My" />
-        <span>마이</span>
+        <img src={mypageIconActive} alt="My" />
       </div>
     </nav>
   );
