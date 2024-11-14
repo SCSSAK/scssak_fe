127378209bde4ed62be726dececce070bbd75c87
조예지 <celpegor216@gmail.com>
2024-11-14 15:39:17 +0900

    rename: 이미지 폴더 구조 변경

--- scssak-fe/src/assets/images/index.js
+++ scssak-fe/src/assets/images/index.js
@@ -1,17 +1,19 @@
 export {default as imgLogo} from './logo.png';
-
 export {default as imgLogin} from './img_login.png';
+
 export {default as backgroundImgSea} from './background_sea.png';
 
 export {default as buttonImgCheck} from './button_check.png';
-export {default as iconSiren} from './icon_siren.png';
 
-export {default as iconEmail} from './icon_email.png';
-export {default as iconSns} from './icon_sns.png';
-export {default as iconIntroduce} from './icon_introduce.png';
-export {default as iconList} from './icon_list.png';
-export {default as iconMenu} from './icon_menu.png';
-export {default as iconSetting} from './icon_setting.png';
+export {default as iconSiren} from './icon/icon_siren.png';
+export {default as iconEmail} from './icon/icon_email.png';
+export {default as iconSns} from './icon/icon_sns.png';
+export {default as iconIntroduce} from './icon/icon_introduce.png';
+export {default as iconList} from './icon/icon_list.png';
+export {default as iconMenu} from './icon/icon_menu.png';
+export {default as iconSetting} from './icon/icon_setting.png';
+export {default as iconComment} from './icon/icon_comment.png';
+export {default as iconHeart} from './icon/icon_heart.png';
 
 export {default as imgMailbox01} from './mailbox/mailbox_01.png';
 export {default as imgMailbox02} from './mailbox/mailbox_02.png';
