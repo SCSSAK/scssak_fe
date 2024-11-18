de5ef47306ccf7e97bd1931320205287f3368b4d
정내혁 <math_star@naver.com>
2024-11-18 10:15:36 +0900

    fix: merge 작업 241118 1015

--- scssak-fe/src/pages/ArticleWritePage.js
+++ scssak-fe/src/pages/ArticleWritePage.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ArticleForm from '../components/article/ArticleForm';
+import {BASE_URL} from '../router/Routes';
 
 const ArticleWritePage = () => {
   const handleSubmit = async formData => {
     try {
-      const response = await fetch('/api/v1/article', {
+      const response = await fetch(BASE_URL + '/article', {
         method: 'POST',
         body: formData,
       });
