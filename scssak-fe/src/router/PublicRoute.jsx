4596e90777934d04e024e66899a01389f56b8456
조예지 <celpegor216@gmail.com>
2024-11-20 16:45:34 +0900

    fix: 로그인 정보가 남아있는 상태에서 url을 입력해서 login 페이지에 접근한 경우 main 페이지로 이동 241120 1645

--- scssak-fe/src/router/PublicRoute.jsx
+++ scssak-fe/src/router/PublicRoute.jsx
@@ -1,14 +1,21 @@
 import {useEffect} from 'react';
-import {useNavigate, Outlet} from 'react-router-dom';
+import {useNavigate, Outlet, useLocation} from 'react-router-dom';
+
+import {mainRoute} from './Routes';
 
 export default function PublicRoute() {
   const isAuthenticated = localStorage.getItem('access_token') !== null;
   const navigate = useNavigate();
+  const location = useLocation();
 
   useEffect(() => {
     // 이미 로그인한 경우 이전 페이지로 돌아감
     if (isAuthenticated) {
-      navigate(-1);
+      if (location.state) {
+        navigate(-1);
+      } else {
+        navigate(mainRoute);
+      }
     }
   }, []);
 
