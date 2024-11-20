bde641b645dcc9172dc5f2eb88717ce5cd30a268
조예지 <celpegor216@gmail.com>
2024-11-20 10:21:03 +0900

    feat: 로그인 여부에 따른 페이지 접근 제한 적용 241120 1020

---
+++ scssak-fe/src/router/ProtectedRoute.jsx
@@ -0,0 +1,30 @@
+import {useEffect} from 'react';
+import {Navigate, useLocation, Outlet} from 'react-router-dom';
+import {useSetRecoilState} from 'recoil';
+import {xModalAtom} from '../recoil/atom';
+
+import {loginRoute} from './Routes';
+
+export default function ProtectedRoute() {
+  // 에러 메시지 전역 상태
+  const setXmodalState = useSetRecoilState(xModalAtom);
+
+  const isAuthenticated = localStorage.getItem('access_token') !== null;
+  const location = useLocation();
+
+  useEffect(() => {
+    // 로그인하지 않은 경우 로그인 페이지로 리다이렉트
+    // state를 통해 이전 위치 정보를 전달하여 로그인 후 원래 가려던 페이지로 이동
+    if (!isAuthenticated) {
+      setXmodalState({
+        isOpened: true,
+        message: '로그인이 필요합니다.',
+      });
+    }
+  }, []);
+
+  if (!isAuthenticated) {
+    return <Navigate to={loginRoute} state={{from: location}} replace />;
+  }
+  return <Outlet />;
+}
